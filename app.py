a4a1f6a8ee427af854d28dad1196e2b805556437
Raul Olivera <raul2402058@hybridge.education>
2024-09-19 13:56:22 -0600

    se anexa materias

--- app.py
+++ app.py
@@ -18,5 +18,16 @@ def estudiantes():
     ]
     return render_template('estudiantes.html', data=data)
 
+@app.route('/materias')
+def materias():
+    dato = [
+        {"ID": "1101", "Nombre de la materia": "Matematicas", "Horario": "08:00 - 09:00", "Profesor": "Gerardo Mathus"},
+        {"ID": "1102", "Nombre de la materia": "Historia", "Horario": "10:00 - 11:00", "Profesor": "Anabel Díaz"},
+        {"ID": "1103", "Nombre de la materia": "Biologia", "Horario": "11:00 - 12:00", "Profesor": "Eduardo Basurto"},
+        {"ID": "1104", "Nombre de la materia": "Fisica", "Horario": "12:00 - 13:00", "Profesor": "Dominica Ferrís"},
+        {"ID": "1105", "Nombre de la materia": "Quimica", "Horario": "13:00 - 14:00", "Profesor": "Luis Dominguez"}
+    ]
+    return render_template('materias.html', dato=dato)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
